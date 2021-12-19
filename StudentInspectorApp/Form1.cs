17f7153b44f2e25324cba6713153bdb94f575376
Jorge García Ochoa de Aspuru <bardok@gmail.com>
2021-12-19 16:43:09 +0100

    Added GPLv3 License.
    Changed the About... dialog's text.

--- StudentInspectorApp/Form1.cs
+++ StudentInspectorApp/Form1.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+     This file is part of StudentInspector.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+    */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -264,8 +281,10 @@ namespace StudentInspectorApp
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show(@"Student Audit Software
-(c) Jorge García Ochoa de Aspuru
-Maristak Bilbao","Acerca de...");
+Jorge García Ochoa de Aspuru.
+Desarrollado para Maristak Bilbao.
+Licenciado bajo GPLv3.
+https://github.com/malkiah/StudentInspector", "Acerca de...");
         }
 
         private void btnOcultar_Click(object sender, EventArgs e)
